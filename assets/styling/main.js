ce12df0ceaf9f57085d4ad3853f6546c6751c466
Fightingpainter <122602731+Fightingpainter@users.noreply.github.com>
2023-06-05 14:27:07 +0200

    adds

--- assets/styling/main.js
+++ assets/styling/main.js
@@ -1,45 +1,89 @@
-indow.onload = function() {
+function createWidget(link, title, text, imgPath, imgAlt) {
+    // Create elements
+    var widget = document.createElement('div');
+    var closeBtn = document.createElement('a');
+    var linkElem = document.createElement('a');
+    var contentDiv = document.createElement('div');
+    var titleH4 = document.createElement('h4');
+    var textP = document.createElement('p');
+    var imgElem = document.createElement('img');
 
-    // Create a new div element
-    var newDiv = document.createElement("div");
+    // Set attributes and contents
+    widget.id = 'my-add';
+    closeBtn.id = 'close-add';
+    closeBtn.className = 'my-add-close';
+    closeBtn.textContent = 'x';
+    linkElem.href = link;
+    contentDiv.className = 'my-add';
+    titleH4.className = 'my-add-title';
+    titleH4.textContent = title;
+    textP.className = 'my-add-text';
+    textP.textContent = text;
+    imgElem.className = 'my-add-image';
+    imgElem.src = imgPath;
+    imgElem.alt = imgAlt;
 
-    // Assign styles to the new div element
-    newDiv.style.position = 'fixed';
-    newDiv.style.top = '0';
-    newDiv.style.right = '0';
-    newDiv.style.width = '200px';
-    newDiv.style.height = '200px';
-    newDiv.style.backgroundColor = 'yellow';
-    newDiv.style.border = '1px solid black';
-    newDiv.style.padding = '10px';
-    newDiv.style.boxSizing = 'border-box';
-    newDiv.style.display = 'flex';
-    newDiv.style.alignItems = 'center';
-    newDiv.style.justifyContent = 'center';
-    newDiv.style.textAlign = 'center';
+    // Build widget structure
+    contentDiv.appendChild(titleH4);
+    contentDiv.appendChild(textP);
+    contentDiv.appendChild(imgElem);
+    linkElem.appendChild(contentDiv);
+    widget.appendChild(closeBtn);
+    widget.appendChild(linkElem);
 
-    // Add some text content to the new div
-    newDiv.innerHTML = "<p>This is an ad</p>";
+    // Append widget to body
+    document.body.appendChild(widget);
 
-    // Create a close button
-    var closeButton = document.createElement("button");
-
-    // Add text to the close button
-    closeButton.innerHTML = "X";
-
-    // Position the close button at the top right corner of the div
-    closeButton.style.position = 'absolute';
-    closeButton.style.top = '5px';
-    closeButton.style.right = '5px';
-
-    // Add an event listener to the close button to hide the div when clicked
-    closeButton.onclick = function() {
-        newDiv.style.display = 'none';
-    };
-
-    // Append the close button to the new div
-    newDiv.appendChild(closeButton);
-
-    // Append the new div to the body of the document
-    document.body.appendChild(newDiv);
+    // Add event listener to 'close' button
+    closeBtn.addEventListener('click', function() {
+        widget.remove();
+    });
 }
+
+function createAdd(adsData) {
+
+    // Wait a random amount of time between 0 and 1 minute (you can adjust this as necessary)
+
+    var delay = Math.random() * 60000;
+
+    setTimeout(function() {
+        // Select a random ad data from the array
+        var adData = adsData[Math.floor(Math.random() * adsData.length)];
+
+        // Call createWidget function with ad data
+        createWidget(adData.link, adData.title, adData.text, adData.imgPath, adData.imgAlt);
+    }, delay);
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Create array of ad data
+    var ads = [
+        {
+            link: "https://fightingpainter.github.io/ZooWebsite/index.html",
+            title: "Evade's Enchanted Aquarium",
+            text: "Dive into the Wonder of Evade's Enchanted Aquarium: Experience Aquatic Beauty Online!",
+            imgPath: "https://fightingpainter.github.io/ZooWebsite/favicon.png",
+            imgAlt: "Evade's Enchanted Aquarium's icon"
+        },
+        {
+            link: "https://standbee.github.io/peculiar-zoo",
+            title: "Zoo of Peculiarity",
+            text: "Discover the Extraordinary at the Zoo of Peculiarity: Unveil the Wonders of Bizarre Wildlife!",
+            imgPath: "https://standbee.github.io/peculiar-zoo/favicon.png",
+            imgAlt: "Zoo of Peculiarity's icon"
+        },
+        {
+            link: "https://sven1222225.github.io/ardougne-zoo",
+            title: "Ardougne Zoo",
+            text: "Embark on an Adventure at Ardougne Zoo: Experience the Thrills of RuneScape's Wildlife Sanctuary!",
+            imgPath: "https://sven1222225.github.io/ardougne-zoo/favicon.png",
+            imgAlt: "Ardougne Zoo's icon"
+        }
+    ];
+
+    // Call createAdd function with ads array
+    createAdd(ads);
+});
